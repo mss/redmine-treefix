6d9ba5d4f84e05e93f871352be6755ae4e5d9bc9
Malte S. Stretz <mss@apache.org>
2011-11-07 12:06:15 +0100

    Fixup stuff.

--- redmine-treefix.pl
+++ redmine-treefix.pl
@@ -1,15 +1,18 @@
 use strict;
 use warnings;
 use 5.010;
-use Dumper;
+
+use Getopt::Std;
+use Data::Dumper;
 
 use DBI;
 
 my $database = "redmine";
 my $username = "root";
-my $password = "password";
+my $password = "";
 
 my $table    = "projects";
+my $sortby   = "name";
 
 
 my @rows;
@@ -25,43 +28,62 @@ my $tree = {
 
 my $count = 0;
 
+sub parse_args;
+sub fetch_data;
+sub build_tree;
+sub update_tree;
+sub dump_sql;
+
 sub main {
+    parse_args;
+
     @rows = fetch_data;
 
     $tree = build_tree;
+    print Dumper($tree);
     $tree = update_tree;
 
-    print Dumper($tree);
+    
     dump_sql;
 }
 
+sub parse_args {
+    my %opts;
+    getopts('d:u:p:t:', \%opts) or die;
+    $database = $opts{'d'} if $opts{'d'};
+    $username = $opts{'u'} if $opts{'u'};
+    $password = $opts{'p'} if $opts{'p'};
+    $table    = $opts{'t'} if $opts{'t'};
+    $sortby   = $opts{'s'} if $opts{'s'};
+}
+
 sub fetch_data {
-    my $dbh = DBD->connect(
+    my $dbh = DBI->connect(
         "DBI:mysql:$database",
         $username,
         $password,
-    ) or die "Could not connect to $database: $DBI::errstr";
+    ) or die;
 
-    my $rows = $dbh->selectall_arrayref(
-        'SELECT id,parent_id,lft,rgt,identifier,name FROM ? ORDER BY id ASC',
+    my $rows = $dbh->selectall_hashref(
+        'SELECT id,parent_id,lft,rgt,identifier,name FROM ' . $table,
+        'id',
         undef,
-        $table,
-    );
+    ) or die;
 
     $dbh->disconnect;
 
-    return @{$rows};
+    return sort {
+            $a->{$sortby} cmp $b->{$sortby}
+        } values %{$rows};
 }
 
 sub build_tree {
     my $root = shift || $tree;
 
-    my @children = sort {
-            $a->{'identifier'} cmp $b->{'identifier'}
-        } grep {
+    my @children = grep {
             $_->{'parent_id'} ~~ $root->{'id'}
         } @rows;
-    map build_tree @children;
+    map { build_tree $_ } @children;
 
     $root->{'children'} = \@children;
     return $root;
@@ -73,7 +95,7 @@ sub update_tree {
     $root->{'lft_old'} = $root->{'lft'};
     $root->{'lft'}     = $count++;
 
-    map update_tree @{$root->{'children'}};
+    map { update_tree $_ } @{$root->{'children'}};
 
     $root->{'rgt_old'} = $root->{'rgt'};
     $root->{'rgt'}     = $count++;
@@ -83,12 +105,12 @@ sub dump_sql {
     printf("USE %s;\n",
         $database
     );
-    printf("START TRANSACTION;\n");
+    printf("START TRANSACTION;\n\n");
 
-    my $p = 1 + int(log($row[-1]->{'id'}) / log(10));
+    my $p = 1 + int(log($rows[-1]->{'id'}) / log(10));
     my $d = "0${p}d";
     foreach my $row (@rows) {
-        printf("/* (%$d, %$d): (%$d, %$d) -> (%$d, %$d) %s: %s */\n",
+        printf("/* (%$d, %$d): (%$d, %$d) -> (%$d, %$d) %s: '%s' */\n",
             $row->{'parent_id'} || 0,
             $row->{'id'},
             $row->{'lft_old'},
@@ -106,7 +128,7 @@ sub dump_sql {
         );
     }
 
-    printf("COMMIT;\n");
+    printf("\nCOMMIT;\n");
 }
 
 
